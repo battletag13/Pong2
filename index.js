02e709157ef2e3da6610d1f54956bfd7926b90bf
battletag13 <firstunity12@gmail.com>
2018-05-11 15:57:29 -0700

    Test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let player, ai, ball;
 
+let trails = [], trailSize = 3, trailTime = 0.5, trailInterval = 0.1;
+
 let downPressed, upPressed;
 
 let width = 1000, height = 500;
@@ -42,6 +44,19 @@ class Ball
 		this.graphics = new RectObj(x, y, width, height);
 		this.velX = velX;
 		this.velY = velY;
+	}
+
+	draw() { this.graphics.draw(true); }
+}
+
+class TrailParticle
+{
+	constructor(size, time)
+	{
+		this.size = size;
+		this.time = time;
+		this.graphics = new RectObj(ball.graphics.x, ball.graphics.y, size, size);
+		setTimeout(function() { this = undefined; }, time * 1000);
 	}
 
 	draw() { this.graphics.draw(true); }
@@ -171,6 +186,14 @@ function drawShapes()
 	player.draw();
 	ai.draw();
 	ball.draw();
+
+	for (let i = 0; i < trails.length; ++i)
+	{
+		if (trails[i] !== undefined)
+		{
+			trails[i].draw();
+		}
+	}
 }
 
 function resetGame() { ball = new Ball(500, height / 2, ballRadius, ballRadius, ballVel, ballVel); }
@@ -198,7 +221,15 @@ function randColor()
 	paddleColor = color(random(0, 255), random(0, 255), random(0, 255));
 }
 
+function trailHandler()
+{
+	for (let i = 0; trails[i] !== undefined; ++i)
+	{
+		trails[i] = new TrailParticle(trailSize, trailTime);
+	}
+}
 
+setInterval(trailHandler, trailInterval * 1000);
 //HTML INTERACTION
 let plrSensitivitySlider = document.getElementById('playerSensitivity');
 let volumeSlider = document.getElementById('volumeLevel');
